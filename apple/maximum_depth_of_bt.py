ad6115e01184e74eb180b3446e9b83d1266d9579
Nirmal Thapa <nthapa@patreon.com>
2020-05-05 03:00:12 -0700

    lowest common ancestor

--- apple/maximum_depth_of_bt.py
+++ apple/maximum_depth_of_bt.py
@@ -10,12 +10,14 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        pass
+        if not root:
+            return 0
+        return max(1+self.maxDepth(root.left), 1+ self.maxDepth(root.right))
 
 
 a = build_binary_tree_T([1,2,3,4])
 b = build_binary_tree_T([])
 c = build_binary_tree_T([1,2,3,5,6,7,8])
 assert Solution().maxDepth(a) == 2
-assert Solution().isSameTree(a) == 0
-assert Solution().isSameTree(c) == 3
+assert Solution().maxDepth(b) == 0
+assert Solution().maxDepth(c) == 3
