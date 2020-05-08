db370bdc3ca49426d91f737434dbdb9399cfe6ea
Nirmal Thapa <nthapa@patreon.com>
2020-05-08 04:58:10 -0700

    new problems

---
+++ apple/median_of_two_sorted_arrays.py
@@ -0,0 +1,13 @@
+
+
+
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+
+
+assert Solution().findMedianSortedArrays([1,2],[3,4]) == 2.5
+assert Solution().findMedianSortedArrays([1,3],[2]) == 2.0
